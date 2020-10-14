8a8a13241dc694337c75bb24e3604e81d50e8687
Filipovich Katerina <k.filipovich@city-mobil.ru>
2020-10-15 00:21:16 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by k.filipovich on 15/10/2020.
+//  Copyright © 2020 k.filipovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
